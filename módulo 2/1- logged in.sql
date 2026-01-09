33257d5085c6c527166255a4c9ffc34689d6701f
Lucas Paguetti Pereira <lucaspaguetti2@gmail.com>
2026-01-09 18:34:14 -0300

    update bd

--- módulo 2/1- logged in.sql
+++ módulo 2/1- logged in.sql
@@ -1,6 +1,15 @@
-INSERT INTO pessoas (nome, idade, genero) VALUES
-('Lucas', 20, "homem"),
-('Maria', 25, "mulher"),
-('João', 30, "homem");
+CREATE DATABASE dados_pessoais;
 
-SELECT * FROM pessoas
+USE dados_pessoais
+
+CREATE TABLE pessoas (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100),
+    idade INT,
+    genero VARCHAR(20),
+);
+INSERT INTO pessoas (nome, idade, genero) VALUES
+('Lucas', 20, 'homem'),
+('Maria', 25, 'mulher'),
+('João', 30, 'homem',);
+

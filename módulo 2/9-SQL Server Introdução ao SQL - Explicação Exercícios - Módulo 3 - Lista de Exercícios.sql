04b52bedb8247619ddbd83fa1a85fff5b464eb2d
Lucas Paguetti Pereira <lucaspaguetti2@gmail.com>
2026-01-24 22:40:10 -0300

    bd added

---
+++ módulo 2/9-SQL Server Introdução ao SQL - Explicação Exercícios - Módulo 3 - Lista de Exercícios.sql
@@ -0,0 +1,130 @@
+-- =========================================================
+-- [SQL Server] Introdução ao SQL
+-- Módulo 3 – Lista de Exercícios
+-- =========================================================
+-- Instruções:
+-- Utilize boas práticas de organização e comentários no código
+-- Escreva as resoluções nos espaços indicados abaixo
+-- =========================================================
+
+
+
+-- =========================================================
+-- EXERCÍCIO 1
+-- Controle de dados de clientes e produtos
+-- =========================================================
+
+-- 1.a Verificar se existem exatamente 2.517 produtos cadastrados
+-- Caso o número seja diferente, reportar ao gestor
+--
+-- Resolução:
+--
+--
+--
+
+
+-- 1.b Até o mês passado existiam 19.500 clientes na base
+-- Verificar se esse número aumentou ou diminuiu
+--
+-- Resolução:
+--
+--
+--
+
+
+
+-- =========================================================
+-- EXERCÍCIO 2
+-- Ação de marketing para aniversariantes
+-- =========================================================
+
+-- Objetivo: Listar clientes com data de nascimento e contato
+
+-- 2.a Selecionar as colunas:
+-- CustomerKey, FirstName, EmailAddress, BirthDate
+--
+-- Resolução:
+--
+--
+--
+
+
+-- 2.b Renomear as colunas utilizando alias (AS)
+--
+-- Resolução:
+--
+--
+--
+
+
+
+-- =========================================================
+-- EXERCÍCIO 3
+-- Ação comemorativa de 10 anos da Contoso
+-- =========================================================
+
+-- 3.a Retornar os 100 primeiros clientes da história
+-- Selecionar todas as colunas da tabela dimCustomer
+--
+-- Resolução:
+--
+--
+--
+
+
+-- 3.b Retornar os primeiros 20% dos clientes da história
+-- Utilizando TOP PERCENT
+--
+-- Resolução:
+--
+--
+--
+
+
+-- 3.c Retornar apenas as 100 primeiras linhas
+-- Selecionando somente:
+-- FirstName, EmailAddress, BirthDate
+--
+-- Resolução:
+--
+--
+--
+
+
+-- 3.d Renomear as colunas anteriores para nomes em português
+--
+-- Resolução:
+--
+--
+--
+
+
+
+-- =========================================================
+-- EXERCÍCIO 4
+-- Contato com fornecedores
+-- =========================================================
+
+-- Objetivo: Retornar apenas os nomes dos fornecedores
+-- Renomeando a coluna resultante
+--
+-- Resolução:
+--
+--
+--
+
+
+
+-- =========================================================
+-- EXERCÍCIO 5
+-- Investigação de produtos não vendidos
+-- =========================================================
+
+-- Objetivo: Verificar se existe algum produto registrado
+-- que ainda não tenha sido vendido
+-- Não é necessário identificar qual produto
+--
+-- Resolução:
+--
+--
+--

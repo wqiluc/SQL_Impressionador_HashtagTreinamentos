e112396c2a8d40c7154222707871ba7d6218bd57
Lucas Paguetti Pereira <lucaspaguetti2@gmail.com>
2026-02-08 10:42:23 -0300

    exércicios 8/10✅🖋️

--- módulo 5/38-Exércicios_do_módulo.sql
+++ módulo 5/38-Exércicios_do_módulo.sql
@@ -113,64 +113,206 @@ LIMIT
 -- a) Descubra qual cliente (CustomerKey)
 -- mais realizou compras online
 -- baseado em SalesQuantity
---
+
+SELECT 
+    * 
+FROM 
+    FACTONLINESALES;
+
+SELECT 
+    MAX(SalesQuantity) as "Máximo de Compras Online"
+FROM
+    FACTONLINESALES
+GROUP BY
+    SalesQuantity
+ORDER BY
+    SalesQuantity DESC
+LIMIT 
+    1;
+
 -- b) Descubra os TOP 3 produtos
 -- mais comprados por esse cliente
--- =========================================================
 
-
+SELECT DISTINCT
+    ProductKey as "ID do Produto",
+    MAX(SalesQuantity) as "Máximo de Compras Online"
+FROM
+    FACTONLINESALES
+GROUP BY
+    ProductKey
+ORDER BY
+    ProductKey
+LIMIT
+    3;
 
 -- =========================================================
 -- 🧠 EXERCÍCIO 4 — DIMPRODUCT 📦
 -- =========================================================
 -- a) Descubra a quantidade total
 -- de produtos por marca
---
+
+SELECT 
+    *
+FROM
+    DimProduct;
+
+SELECT DISTINCT
+    ProductKey as "ID do Produto",
+    BrandName as "Nome da Marca do Produto",
+    UnitPrice as "Preço Unitário (U$D)"
+FROM
+    DimProduct
+ORDER BY
+    BrandName;
+
 -- b) Descubra a média do preço
 -- (UnitPrice) por ClassName
---
+
+SELECT DISTINCT
+    ClassName as "Classe de Nome do Produto",
+    AVG(UnitPrice) as "Média de Preço Unitário do Produto (U$D)"
+FROM
+    DimProduct
+GROUP BY
+    ClassName
+ORDER BY
+    ClassName DESC;
+
 -- c) Descubra o peso total
 -- por cor de produto
--- =========================================================
 
-
+SELECT DISTINCT
+    ColorName as "Cor do Produto",
+    Weight as "Peso do Produto"
+FROM
+    DimProduct
+ORDER BY
+    ColorName DESC;
 
 -- =========================================================
 -- 🧠 EXERCÍCIO 5 — FILTRO + ORDER BY 🏷️
 -- =========================================================
--- Descubra o peso total por tipo
+-- a)Descubra o peso total por tipo
 -- de produto (StockTypeName)
---
--- Considerar apenas:
+
+SELECT 
+    *
+FROM
+    DimProduct;
+
+SELECT DISTINCT
+    StockTypeName as "Tipo de Estoque do Produto",
+    MAX(Weight) as "Peso total dos Produtos"
+FROM
+    DimProduct
+GROUP BY
+    StockTypeName
+ORDER BY
+    StockTypeName ASC;
+
+-- b)Considerar apenas:
 -- marca = 'Contoso'
---
--- Ordenar em ordem decrescente
--- =========================================================
 
+SELECT DISTINCT
+    BrandName as "Nome da Marca do Produto",
+    StockTypeName as "Tipo de Estoque do Produto",
+    MAX(Weight) as "Peso total dos Produtos"
+FROM
+    DimProduct
+WHERE
+    BrandName = "Contoso"
+GROUP BY
+    StockTypeName
+ORDER BY
+    StockTypeName ASC;
 
+-- c)ordenar em ordem decrescente
+
+SELECT DISTINCT
+    BrandName as "Nome da Marca do Produto",
+    StockTypeName as "Tipo de Estoque do Produto",
+    MAX(Weight) as "Peso total dos Produtos"
+FROM
+    DimProduct
+WHERE
+    BrandName = "Contoso"
+GROUP BY
+    StockTypeName
+ORDER BY
+    StockTypeName DESC;
 
 -- =========================================================
 -- 🧠 EXERCÍCIO 6 — VALIDAÇÃO DE CORES 🎨
 -- =========================================================
--- Verifique se todas as marcas
+-- a)Verifique se todas as marcas
 -- possuem as 16 opções de cores
 -- disponíveis
 -- =========================================================
 
+SELECT 
+    *
+FROM
+    DimProduct;
 
+SELECT DISTINCT
+    BrandName AS "Nome do Produto",
+    ColorName AS "Cores dos Produtos"
+FROM
+    DimProduct
+WHERE
+    ColorName IN ('Red','Blue','Black','White','Silver','Yellow','Green','Multi','Grey','Orange','Brown','Purple','Pink','Gold');
 
 -- =========================================================
 -- 🧠 EXERCÍCIO 7 — DIMCUSTOMER 👥
 -- =========================================================
 -- Descubra:
--- - total de clientes por sexo
+-- - total de clientes por sexo✅
 -- - média salarial por sexo
---
--- Corrija possíveis resultados
--- inesperados
--- =========================================================
 
+SELECT 
+    *
+FROM
+    DimCustomer;
 
+/*Gênero Masculino:*/
+SELECT DISTINCT
+    CustomerKey as "ID do Cliente",
+    Gender as "Gênero Masculino"
+FROM
+    DimCustomer
+WHERE
+    Gender = "M";
+
+/*Gênero Feminino:*/
+SELECT DISTINCT
+    CustomerKey as "ID do Cliente",
+    Gender as "Gênero Feminino"
+FROM
+    DimCustomer
+WHERE
+    Gender = "F";
+
+/*Média Salarial Masculina:*/
+SELECT DISTINCT
+    AVG(YearlyIncome) as "Média Salarial Masculina",
+    Gender as "Gênero Masculino"
+FROM
+    DimCustomer
+WHERE
+    Gender = "M"
+GROUP BY
+    YearlyIncome;
+
+/*Média Salarial Feminina:*/
+SELECT DISTINCT
+    AVG(YearlyIncome) as "Média Salarial Feminina",
+    Gender as "Gênero Feminino"
+FROM
+    DimCustomer
+WHERE
+    Gender = "F"
+GROUP BY
+    YearlyIncome;
 
 -- =========================================================
 -- 🧠 EXERCÍCIO 8 — ESCOLARIDADE 🎓
@@ -182,7 +324,6 @@ LIMIT
 -- =========================================================
 
 
-
 -- =========================================================
 -- 🧠 EXERCÍCIO 9 — DIMEMPLOYEE 👨‍💼
 -- =========================================================
@@ -191,7 +332,6 @@ LIMIT
 --
 -- Considerar apenas funcionários ativos
 -- =========================================================
-
 
 
 -- =========================================================
@@ -206,5 +346,4 @@ LIMIT
 --   Marketing, Engineering e Finance
 -- - contratadas entre 1999 e 2000
 -- =========================================================
-
 
